501653d18d08ab16dcf5a51a5d3f8edf58a9888b
Randy Ridenour <rlridenour@gmail.com>
2021-04-26 15:57:50 -0500

    Add handout snippets.

--- elisp/lang-org.el
+++ elisp/lang-org.el
@@ -31,6 +31,16 @@
 			   ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
 			   ("\\paragraph{%s}" . "\\paragraph*{%s}")
 			   ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+(add-to-list 'org-latex-classes
+			 '("org-handout"
+			   "\\documentclass{obuhandout}
+      [NO-DEFAULT-PACKAGES]
+      [NO-PACKAGES]"
+			   ("\\section{%s}" . "\\section*{%s}")
+			   ("\\subsection{%s}" . "\\subsection*{%s}")
+			   ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+			   ("\\paragraph{%s}" . "\\paragraph*{%s}")
+			   ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
 )
 
 ;; (add-hook 'org-mode-hook 'wc-mode)
